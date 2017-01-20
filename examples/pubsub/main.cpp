0bd3facb372457a9c6b8d810f7488635f77cd48b
Petar Koretic <petar.koretic@gmail.com>
2017-01-20 17:08:40 +0100

    examples/pubsub: update to only callback code
    Signed-off-by: Petar Koretic <petar.koretic@gmail.com>

--- examples/pubsub/main.cpp
+++ examples/pubsub/main.cpp
@@ -13,29 +13,13 @@ int main(int argc, char *argv[])
 
     qDebug() << "client connecting";
 
-    client.connect();
-
-    QObject::connect(&client, &NatsClient::connected, [&client]
+    client.connect("127.0.0.1", 4222, [&client]
     {
-        qDebug() << "connected";
-
-        // regular subscribe to subject
-        client.subscribe("foobar", [](auto message, auto reply_inbox, auto subject)
-        {
-            qDebug() << "received message:" << message;
-            qDebug() << "received subject:" << subject;
-            qDebug() << "received reply inbox:" << reply_inbox;
-        });
-
-        // wildcard subscriptions
         client.subscribe("foo*", [](auto message, auto reply_inbox, auto subject)
         {
-            qDebug() << "received message:" << message;
-            qDebug() << "received subject:" << subject;
-            qDebug() << "received reply inbox:" << reply_inbox;
+            qDebug().noquote() << "received message:" << message << subject << reply_inbox;
         });
 
-        // publish message - this will trigger both subscriptions
         client.publish("foobar", "Hello NATS!");
     });
 

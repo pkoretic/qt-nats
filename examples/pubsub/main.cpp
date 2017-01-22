b623c4af562844623a613072ecbfdf1a47bacf7c
Petar Koretic <petar.koretic@gmail.com>
2017-01-22 20:16:01 +0100

    examples/pubsub: remove extra output
    Signed-off-by: Petar Koretic <petar.koretic@gmail.com>

--- examples/pubsub/main.cpp
+++ examples/pubsub/main.cpp
@@ -4,22 +4,18 @@
 
 int main(int argc, char *argv[])
 {
-    setenv("DEBUG", "qt-nats", 1);
-
     QCoreApplication a(argc, argv);
 
     Nats::Client client;
 
-    qDebug() << "client connecting";
-
     client.connect("127.0.0.1", 4222, [&client]
     {
-        client.subscribe("foo*", [](QString message, QString reply_inbox, QString subject)
+        client.subscribe("foo", [](QString message, QString reply_inbox, QString subject)
         {
             qDebug().noquote() << "received message:" << message << subject << reply_inbox;
         });
 
-        client.publish("foobar", "Hello NATS!");
+        client.publish("foo", "Hello NATS!");
     });
 
     return a.exec();
